964b2c8ede7ffac8c70209a846316edb1cde7c6d
nilorenz <niklaslorenz28@gmail.com>
2026-07-13 14:08:44 +0200

    First Commit

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,12 @@
-from src.calculator import add
+from src.calculator import add, subtract
 
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-2, 2) == 0
     assert add(0, 0) == 0
+    
+def test_subtract():
+    assert subtract(1, 2) == -1
+    assert subtract(-2, 2) == -4
+    assert subtract(0, 0) == 0
